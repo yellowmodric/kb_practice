352aaa9ed6c04321fe1bdef4fc2a4b7dc7361602
yellowmodric <kyr7091@naver.com>
2025-03-26 16:15:14 +0900

    집가고싶어요

---
+++ v-model-test/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- 자식에서 변경이 될때마다 name 값이 searchName에 반영됨 -->
+    <InputName v-model:name="searchName" />
+    <h3>검색어 : {{ searchName }}</h3>
+  </div>
+</template>
+
+<script>
+import InputName from './components/InputName.vue';
+
+export default {
+  name: 'App',
+  components: { InputName },
+  data() {
+    return {
+      searchName: 'John',
+    };
+  },
+};
+</script>
